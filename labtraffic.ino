b927d7a97ea9b118ea81f0198e2c567be969fa13
M VAMSHI KRISHNA <71306701+mvamshi33@users.noreply.github.com>
2021-10-08 15:54:16 +0530

    Add files via upload

---
+++ labtraffic.ino
@@ -0,0 +1,27 @@
+int pin_Red=12;
+int pin_Yellow=10;
+int pin_Green=6;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(pin_Red, OUTPUT);
+  pinMode(pin_Yellow, OUTPUT);
+  pinMode(pin_Green, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(pin_Red, HIGH);
+  digitalWrite(pin_Yellow, LOW);
+  digitalWrite(pin_Green, LOW);   
+  delay(1000);   
+                      
+  digitalWrite(pin_Red, LOW);    
+  digitalWrite(pin_Yellow, HIGH);
+  digitalWrite(pin_Green, LOW);
+  delay(1000);    
+                     
+  digitalWrite(pin_Red, LOW);    
+  digitalWrite(pin_Yellow, LOW);
+  digitalWrite(pin_Green, HIGH);    
+  delay(1000);                     
+}
