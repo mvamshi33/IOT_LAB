d26fcf5b4981d597fd689be93c6a522f31163929
M VAMSHI KRISHNA <71306701+mvamshi33@users.noreply.github.com>
2021-10-22 15:33:02 +0530

    Add files via upload

---
+++ button_blink.ino
@@ -0,0 +1,21 @@
+const int buttonPin=2;
+const int ledPin=13;
+
+int buttonState=0;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT); 
+}
+
+// the loop function runs over and over again forever
+void loop() {
+buttonState = digitalRead(butonPin);
+
+if(buttonState==HIGH){
+  digitalWrite(ledPin, HIGH)
+} else{
+digitalWrite(ledPin, LOW);
+}
+}
